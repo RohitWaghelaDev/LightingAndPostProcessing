ab8c2cdb2295a349aa366f147c2ee903ffafe958
RohitWaghela <rohitwagheladev@gmail.com>
2021-05-31 22:36:00 +0530

    different types of light maps added

---
+++ Lighting practice/Assets/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public GameObject PostProcessorVolume;
+    public Text FpsText;
+    private bool showingposetProcessor = true;
+
+    private bool showingFps=false;
+    private void Start()
+    {
+
+        if (FpsText!= null)
+        {
+            FpsText.gameObject.SetActive(false);
+
+        }
+    }
+
+    public void LoadScene(int index) 
+    {
+        Debug.Log("clicked post loadScene");
+        SceneManager.LoadScene(index);
+    }
+
+
+
+    public void SetPostProcessor() 
+    {
+
+        Debug.Log("clicked post processor");
+        if (!showingposetProcessor)
+        {
+            showingposetProcessor = true;
+            PostProcessorVolume.SetActive(true);
+        }
+        else
+        {
+            showingposetProcessor = false;
+            PostProcessorVolume.SetActive(false);
+        }
+    
+    }
+
+
+    public void SetFPS() 
+    {
+        Debug.Log("clicked post FPS");
+        if (!showingFps)
+        {
+            showingFps = true;
+            FpsText.gameObject.SetActive(true);
+            StartCoroutine("ShowFps");
+
+        }
+        else
+        {
+            showingFps = false;
+            StopCoroutine("ShowFps");
+            FpsText.gameObject.SetActive(false);
+           
+        }
+    
+    }
+
+
+    private  IEnumerator  ShowFps()
+    {
+
+        while (true)
+        {
+            float fps = 1 / Time.deltaTime;
+            FpsText.text = Mathf.FloorToInt(fps).ToString();
+            yield return new WaitForSeconds(1);
+        }
+    
+    }
+}
